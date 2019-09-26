43f6753b03e7da2d776b5c8d0ae10c42a5af1541
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-09-26 14:54:00 -0400

    Added Spot Tunes project to repo

---
+++ spot-tunes/spot-tunes/src/main/java/com/example/spottunes/SpotTunesApplication.java
@@ -0,0 +1,13 @@
+package com.example.spottunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotTunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotTunesApplication.class, args);
+	}
+
+}
