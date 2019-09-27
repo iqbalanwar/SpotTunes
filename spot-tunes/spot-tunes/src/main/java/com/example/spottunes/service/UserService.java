69a67ba8438d74fa538d4ef8304317df6b338ab9
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-09-27 18:15:34 -0400

    Created User Service interface

--- spot-tunes/spot-tunes/src/main/java/com/example/spottunes/service/UserService.java
+++ spot-tunes/spot-tunes/src/main/java/com/example/spottunes/service/UserService.java
@@ -1,5 +1,8 @@
 package com.example.spottunes.service;
 
 public interface UserService {
-    
+
+    public User createUser(User newUser);
+
+    public User login(String username, String password);
 }
