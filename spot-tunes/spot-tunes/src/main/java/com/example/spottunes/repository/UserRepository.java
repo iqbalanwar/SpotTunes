35df9cf6633c2b77f0bfe2b2c395dd4d1ce45c38
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-09-27 18:14:06 -0400

    Created user repository

---
+++ spot-tunes/spot-tunes/src/main/java/com/example/spottunes/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.spottunes.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, String> {
+    @Query("FROM User u WHERE u.username = ?1 and u.password = ?2")
+    public User login(String username, String password);
+}
