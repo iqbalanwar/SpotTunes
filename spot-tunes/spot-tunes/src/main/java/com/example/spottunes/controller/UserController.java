94c281e6b4f201d8f3466186376cbb679b68458e
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-09-27 18:10:43 -0400

    Made packages

--- spot-tunes/spot-tunes/src/main/java/com/example/spottunes/controller/UserController.java
+++ spot-tunes/spot-tunes/src/main/java/com/example/spottunes/controller/UserController.java
@@ -7,5 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @GetMapping("/signup")
-    public String createUser(@RequestBody
+    public String createUser(@RequestBody User newUser) {
+        return userService.createUser(newUser);
+    }
 }
