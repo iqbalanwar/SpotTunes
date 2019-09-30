5518809a2076880ce02a975ec88aa735a4693303
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-09-30 18:36:39 -0400

    Created playlist JOIN table

--- spot-tunes/spot-tunes/src/main/java/com/example/spottunes/model/Songs.java
+++ spot-tunes/spot-tunes/src/main/java/com/example/spottunes/model/Songs.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import java.sql.Time;
+import java.util.List;
 
 @JsonIdentityInfo(
         generator = ObjectIdGenerators.PropertyGenerator.class,
@@ -23,6 +24,24 @@ public class Songs {
 
     @Column
     private Time length;
+
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH,
+                    CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name = "playlist",
+            joinColumns = {@JoinColumn(name = "song_id")},
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> users;
+
+    
+    public Songs() {}
+
+
+    public List<User> getUsers(){ return users; }
+
+    public void setUsers(List<User> users) { this.users = users; }
+
 
     public Long getId() {
         return id;
