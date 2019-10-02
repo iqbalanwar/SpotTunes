2c4b1b272f5f394d94dac5f06fbad423e0b57909
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-10-01 20:36:53 -0400

    Tested for login, signup, addSongs and deleteSongs in UserService

---
+++ spot-tunes/spot-tunes/src/test/java/com/example/spottunes/service/UserServiceStub.java
@@ -0,0 +1,58 @@
+package com.example.spottunes.service;
+
+import com.example.spottunes.model.Songs;
+import com.example.spottunes.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.sql.Time;
+
+public class UserServiceStub implements UserService {
+
+    @Override
+    public User getUser(String username) {
+        User user = new User();
+        user.setUsername(username);
+        return user;
+    }
+
+    @Override
+    public String createUser(User newUser) {
+        return null;
+    }
+
+    @Override
+    public String login(User user) {
+        return null;
+    }
+
+    @Override
+    public HttpStatus deleteById(Long userId) {
+        return null;
+    }
+
+    @Override
+    public User addSongs(String username, int song_id) {
+        Songs song = new Songs();
+
+        song.setId(song_id);
+        song.setTitle("Baby Back Ribs");
+        song.setLength(new Time(163000));
+
+        User user = getUser(username);
+        user.addSongs(song);
+
+        return user;
+    }
+
+    @Override
+    public User deleteSongFromPlaylist(String username, int song_id) {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
